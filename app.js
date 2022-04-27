d36513f0614ef5f6cc4e4065bfc8c7ef745740f2
‘AMAYA <aamaya547@gmail.com>
2022-04-27 12:17:25 -0700

    removed console logs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const soda = document.getElementById('soda');
-console.log(soda);
+// console.log(soda);
 const cheese = document.getElementById('cheese');
-console.log(cheese);
+// console.log(cheese);
 const grapes = document.getElementById('grapes');
-console.log(grapes);
+// console.log(grapes);
 const bread = document.getElementById('bread');
-console.log(bread);
+// console.log(bread);
 const watermelon = document.getElementById('watermelon');
-console.log(watermelon);
+// console.log(watermelon);
 
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
