0dd95d9a7fb70bd6edd0fe5492baa254b5d4db2d
‘AMAYA <aamaya547@gmail.com>
2022-04-27 12:10:06 -0700

    grapes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const soda = document.getElementById('soda');
 console.log(soda);
 const cheese = document.getElementById('cheese');
+console.log(cheese);
+const grapes = document.getElementById('grapes');
+console.log(grapes)
 
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
@@ -10,3 +13,6 @@ cheese.addEventListener('click', () => {
     cheese.classList.toggle('picked');
 });
 
+grapes.addEventListener('click', () => {
+    grapes.classList.toggle('picked');
+})
